84fdfd443f00c9b506b1527edf1f1121798c59a8
thanhle <thanh@gmail.com>
2022-09-24 15:15:18 +0700

    Configure ESLint for the project

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
 
 const app = express();
-const ApiError = require('./api-error');
+const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to contact book application.'});
-});
+    res.json({ message: 'Welcome to contact book application.' });
+    });
 app.route('/api/contacts')
     .get(contactController.findAll)
     .post(contactController.create)
@@ -27,14 +27,11 @@ app.use((req, res, next) => {
     // Call next() to pass to the error handling middleware.
     return next(new ApiError(404, 'Resource not found'));
     });
-    // Define error-handling middleware last, after other app.use() and routes calls.
-    app.use((error, req, res, next) => {
-    // The centralized error handling middleware.
-    // In any route handler, calling next(error)
-    // will pass to this error handling middleware.
-        return res.status(error.statusCode || 500).json({
+    // Define error-handling middleware last, after other app.use() and routes
+    app.use((error, req, res) => {
+        return res.status(error.statusCOde || 500).json({
             message: error.message || 'Internal Server Error',
+        });
     });
-});
 
 module.exports = app;
